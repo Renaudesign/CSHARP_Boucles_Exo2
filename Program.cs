c37b4dbd4a669d4ce92068ea4b38a5050e9c3314
renaudesign <renaud.sourmail@gmail.com>
2021-02-17 14:45:38 +0100

    exo2 terminé

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSHARP_Boucles_Exo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+
+            while (count < 11)
+            {
+                Console.WriteLine($"Ceci n'est que le message de test numéro {count}. Veuillez ne pas y prêter attention");
+                count++;
+            }
+        }
+    }
+}
